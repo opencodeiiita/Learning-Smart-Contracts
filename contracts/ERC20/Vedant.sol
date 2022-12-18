c9793652713619043df81f6371abc64dedd97813
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 11:23:12 +0530

    Create Vedant.sol

---
+++ contracts/ERC20/Vedant.sol
@@ -0,0 +1,24 @@
+
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+
+
+contract VedantToken is DetailedERC20, StandardToken {
+  constructor(
+    // The following four parameters need to be specified in migration script.
+    string _name,
+    string _symbol,
+    uint8 _decimals,
+    uint256 _amount
+  )
+    DetailedERC20(_name, _symbol, _decimals)
+    public
+  {
+    require(_amount > 0, "amount has to be greater than 0");
+    totalSupply_ = _amount.mul(10 ** uint256(_decimals));
+    balances[msg.sender] = totalSupply_;
+    emit Transfer(address(0), msg.sender, totalSupply_);
+  }
+}
