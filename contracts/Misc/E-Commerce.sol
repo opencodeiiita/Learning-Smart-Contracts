5dbde75df32f5c2723bc7fb6f474b5de9a5b5488
ayashika <ayashika2624@gmail.com>
2022-12-18 18:41:29 +0530

    All bugs Fixed

--- contracts/Misc/E-Commerce.sol
+++ contracts/Misc/E-Commerce.sol
@@ -32,17 +32,19 @@ contract Ecommerce{
             tempProduct.title = _title;
             tempProduct.description = _description;
             tempProduct.price = _price * 10**18;
-            tempProduct.seller = msg.sender;
+            tempProduct.seller = payable(msg.sender);//BUG: The address of the seller should be a payable address so that it can recieve some ether whenever paid by someone
             tempProduct.productId = counter;
             products.push(tempProduct);
+            counter++; //BUG: This statement is needed to increment the ID of new product added so that each product gets unique ID.
             emit registered(_title,tempProduct.productId, msg.sender);
         }    
         
      // funtion to buy the product
-    function buy(uint _productId) public{
-        require(products[_productId].price > msg.value,"Please pay the exact amount");
+    function buy(uint _productId) payable public{// BUG: buy function should be payable otherwise it will not allow the buyer to send ethers to the seller
+        require(products[_productId].price == msg.value,"Please pay the exact amount");// BUG: In require statement, the price of the product should be exactly equal to the value paid by the buyer and not greater than that
+        require(products[_productId].seller!=msg.sender,"Seller cannot be the buyer");// BUG: This statement is required as the one who is registering the product cannot buy it
         products[_productId].buyer  = msg.sender;
-        emit bought(_productId);
+        emit bought(_productId, msg.sender); //BUG: The second parameter i.e address of the buyer was not passed
     }
  
     // function to confirm delivery from buyer
