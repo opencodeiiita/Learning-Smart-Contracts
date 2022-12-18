0df903dc0cf243a1de7ba01220fa7fe1a3daf033
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 19:05:41 +0530

    Update Vedant.sol

--- contracts/ERC20/Vedant.sol
+++ contracts/ERC20/Vedant.sol
@@ -1,19 +1,17 @@
 
-pragma solidity 0.4.24;
+pragma solidity 0.8.17;
 
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
-contract VedantToken is DetailedERC20, StandardToken {
+contract VedantToken is ERC20 {
   constructor(
-    // The following four parameters need to be specified in migration script.
     string _name,
     string _symbol,
     uint8 _decimals,
     uint256 _amount
   )
-    DetailedERC20(_name, _symbol, _decimals)
+    ERC20(VedantToken, "Tooploox", 18, 21000000)
     public
   {
     require(_amount > 0, "amount has to be greater than 0");
