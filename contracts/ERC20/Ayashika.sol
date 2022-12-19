f59a866825c3e6f33be1f6331e6373bd62628a6d
tsunami03 <priyadarshini.roy03@gmail.com>
2022-12-19 10:51:33 +0530

    compilation issues fixed

--- contracts/ERC20/Ayashika.sol
+++ contracts/ERC20/Ayashika.sol
@@ -2,7 +2,7 @@
 // contracts Ayashika.sol
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Ayashika is ERC20 {
     constructor(uint initialSupply) ERC20("Ayashika", "AYG") {
