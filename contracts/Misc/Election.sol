e29f3975a8d65f7d4b08e1fec913aa1eafbad106
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-21 02:09:53 +0530

    Election system added by SanyamAgrawal07

--- contracts/Misc/Election.sol
+++ contracts/Misc/Election.sol
@@ -5,29 +5,29 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Election{
     mapping(address => bool) voted;
     mapping(string => uint) candidatesVotes;
+    mapping(string => bool) candidates;
     string[] winners;
-    uint currentMax=1;
+    uint currentMax=0;
     address owner;
 
     // Taking the list of "strings" input and adding them to our mapping
     constructor(string[] memory list){
         owner=msg.sender;
         for(uint i=0 ; i<list.length ; i++){
-            candidatesVotes[list[i]]=1;
+            candidatesVotes[list[i]]=0;
+            candidates[list[i]]=true;
         }
     }
 
     function vote(string memory _candidate) external {
-        require(msg.sender!=owner);     // Make sure owner cant vote
-        require(voted[msg.sender]!=true);   // Make sure voter has not already voted
-        require(candidatesVotes[_candidate]>0); // Make sure the candidate name is valid
+        require(msg.sender!=owner,"The owner of the contract cannot vote");     // Make sure owner cant vote
+        require(voted[msg.sender]!=true,"You have already voted in the election");   // Make sure voter has not already voted
+        require(candidates[_candidate]==true,"Invalid candidate name"); // Make sure the candidate name is valid
         candidatesVotes[_candidate]++;
         voted[msg.sender]=true;
         if(candidatesVotes[_candidate]>currentMax){ // Logic to determine winner
             currentMax=candidatesVotes[_candidate];
-            for(uint i=0 ; i<winners.length ; i++){
-                winners.pop();
-            }
+            delete winners;
             winners.push(_candidate);
         }
         else if(candidatesVotes[_candidate]==currentMax){    // Multiple winners if they have equal votes
@@ -37,6 +37,6 @@ contract Election{
 
     function winner() external view returns (string[] memory,uint){  // Returns the results
         uint temp = candidatesVotes[winners[0]];
-        return (winners,temp-1);
+        return (winners,temp);
     }
 }
