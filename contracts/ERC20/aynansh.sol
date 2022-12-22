e573b2426418760be75d31381729884f9a1b9118
tsunami03 <priyadarshini.roy03@gmail.com>
2022-12-22 12:28:48 +0530

    compiled

--- contracts/ERC20/aynansh.sol
+++ contracts/ERC20/aynansh.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Aynansh is ERC20 {
     constructor() ERC20("Aynansh", "AYK") {
         _mint(msg.sender,1000*10**18);
-    }
+    }
 }
