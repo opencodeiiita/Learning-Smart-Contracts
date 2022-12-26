f840390dddc9e643e830d017b621e15680bdb526
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-26 12:30:32 +0530

    Marriage contract done

--- contracts/Misc/Marriage.sol
+++ contracts/Misc/Marriage.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Marriage{
 
-    string status="Single";
+    enum state {Single,Married,Divorced}
+    state status=state.Single;
     address payable zeroAddress = payable(0x0);
-    address payable proposer=zeroAddress; 
+    address payable proposer=zeroAddress;
     address payable proposed=zeroAddress;
     address payable divorcer=zeroAddress;
     address payable divorcee=zeroAddress;
@@ -20,27 +21,32 @@ contract Marriage{
     constructor(){
 
     }
-    
+
+    // The function through which a person can propose another person
     function propose(address payable _proposed) public {
         require(proposer==zeroAddress,"Proposal has already been made");
         proposer=payable(msg.sender);
         proposed=_proposed;
     }
 
-    function getStatus() public view returns(string memory){
+    // Returns the status of the contract
+    function getStatus() public view returns(state){
         return status;
     }
 
+    // The person who has been proposed can accept the offer
     function acceptProposal() public {
         require(payable(msg.sender)==proposed,"You are not the one who was proposed");
-        status="Married";
+        status=state.Married;
     }
 
+    // Returns the address of this contract
     function returnJointAccountAddress() public view returns(address){
         require((payable(msg.sender)==proposed) || (payable(msg.sender)==proposer),"You are not involved in the marriage");
         return address(this);
     }
 
+    // The couple deposits money for their joint account
     function Deposit() public payable {
         require(keccak256(abi.encodePacked(status))==keccak256(abi.encodePacked("Married")),"The status of contract is not married");
         require((payable(msg.sender)==proposed) || (payable(msg.sender)==proposer),"You are not involved in the marriage");
@@ -53,6 +59,7 @@ contract Marriage{
         }
     }
 
+    // One of them could ask for divorce
     function Divorce() public {
         require(deposit1 && deposit2,"You cannot ask for divorce if joint account not made");
         require((payable(msg.sender)==proposed)||(payable(msg.sender)==proposer),"You are not involved in the marriage");
@@ -64,10 +71,11 @@ contract Marriage{
 
     function acceptDivorce() public {
         require(payable(msg.sender)==divorcee,"You are not the one who was asked for divorce");
-        status="Divorced";
+        status=state.Divorced;
         split();
     }
 
+    // Called from the function only, transfers the joint account balance to the ex-couple's accounts
     function split() public payable {
         proposed.transfer((address(this).balance)/2);
         proposer.transfer((address(this).balance)/2);
